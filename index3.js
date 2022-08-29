3448ba8d0d7adf6bfe135a59a6315e26c91ee3c7
karamveer-hub <68920957+karamveer-hub@users.noreply.github.com>
2022-08-29 14:48:17 -0700

    Update index3.js

--- index3.js
+++ index3.js
@@ -21,9 +21,10 @@ function onSignIn(googleUser) {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
+         $('.g-signin').css('display','block')
+    $('.data').css('display','block')
     });
 
-    $('.g-signin').css('display','block')
-    $('.data').css('display','block')
+  
 
   }
